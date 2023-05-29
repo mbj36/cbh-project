d9acf9630081c8cab52d6aaebe2cc8a22c8e622e
mbj36 <mohitbajo36@gmail.com>
2023-05-30 00:28:46 +0530

    test: add more tests for function deterministicPartitionKey

--- dpk.test.js
+++ dpk.test.js
@@ -25,4 +25,11 @@ describe('deterministicPartitionKey', () => {
       '2884b50599fb0a839bca31ebbd9195bf5185434ca3c5ad901ede3c55902a01a60224d8a6a933d846afdf1deb245ea64f1b4b1f9db8f49b6f276d4999d4073e4c'
     );
   });
+  it('if candidate length exceeds max partition key length', () => {
+    const event = { partitionKey: 'abc'.repeat('86') };
+    const trivialKey = deterministicPartitionKey(event);
+    expect(trivialKey).toBe(
+      'a9487a9077759e3bfe689cf06db37371db2a8ce0478ff14835dbb420e8ab0e4da0955f415bc55ecce86d17f2ef2b8d90e7539dcb4a05cc05be5c5ae99ff82803'
+    );
+  });
 });
